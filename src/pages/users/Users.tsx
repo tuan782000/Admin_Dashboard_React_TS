4d7e9cec91e63ae8302439f87082a34fff4bea7d
tuan782000 <thaituan7820@gmail.com>
2023-12-19 18:13:51 +0700

    React MUI DataGrid Tutorial

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,8 +1,124 @@
+import { GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import DataTable from '../../components/dataTable/DataTable'
 import './users.scss'
+import { userRows } from '../../data';
+
+// const columns: GridColDef[] = [
+//   { field: "id", headerName: "ID", width: 90 },
+//   { field: "avatar", headerName: "Avatar", width: 100, 
+//     renderCell: (params) => {
+//       return <img src={params.row.img || "/noavatar.png"} />
+//     } 
+//   },
+//   { field: "actions", headerName: "Actions", width: 100, 
+//     renderCell: (params) => {
+//       return <div className="action">
+//           <div className="view">View</div>
+//           <div className="delete">Delete</div>
+//       </div>
+//     } 
+//   },
+//   { field: "status", headerName: "Status", width: 100, type: "Boolean"
+//   },
+//   {
+//       field: "firstName",
+//       headerName: "First name",
+//       width: 150,
+//       editable: true,
+//   },
+//   {
+//       field: "lastName",
+//       headerName: "Last name",
+//       width: 150,
+//       editable: true,
+//   },
+//   {
+//       field: "age",
+//       headerName: "Age",
+//       type: "number",
+//       width: 110,
+//       editable: true,
+//   },
+//   {
+//       field: "fullName",
+//       headerName: "Full name",
+//       description: "This column has a value getter and is not sortable.",
+//       sortable: false,
+//       width: 160,
+//       valueGetter: (params: GridValueGetterParams) =>
+//           `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+//   },
+// ];
+
+// const rows = [
+//   { id: 1, lastName: "Snow", firstName: "Jon", age: 14, status: true },
+//   { id: 2, lastName: "Lannister", firstName: "Cersei", age: 31, status: false },
+//   { id: 3, lastName: "Lannister", firstName: "Jaime", age: 31 },
+//   { id: 4, lastName: "Stark", firstName: "Arya", age: 11 },
+//   { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+//   { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+//   { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+//   { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+//   { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+// ];
+
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 50 },
+  {
+    field: "img",
+    headerName: "Avatar",
+    width: 100,
+    renderCell: (params) => {
+      return <img src={params.row.img || "/noavatar.png"} alt="" />;
+    },
+  },
+  {
+    field: "firstName",
+    type: "string",
+    headerName: "First name",
+    width: 100,
+  },
+  {
+    field: "lastName",
+    type: "string",
+    headerName: "Last name",
+    width: 150,
+  },
+  {
+    field: "email",
+    type: "string",
+    headerName: "Email",
+    width: 200,
+  },
+  {
+    field: "phone",
+    type: "string",
+    headerName: "Phone",
+    width: 100,
+  },
+  {
+    field: "createdAt",
+    headerName: "Created At",
+    width: 100,
+    type: "string",
+  },
+  {
+    field: "verified",
+    headerName: "Verified",
+    width: 150,
+    type: "boolean",
+  },
+];
 
 const Users = () => {
   return (
-    <div className='users'>Users</div>
+    <div className='users'>
+      <div className="info">
+        <h1>Users</h1>
+        <button>Add New User</button>
+      </div>
+      <DataTable slug="users" columns={columns} rows={userRows}/>
+    </div>
   )
 }
 
